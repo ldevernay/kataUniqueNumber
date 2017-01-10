0f1b1cf814054d93e657cc3c56fd2597efd2e710
ldevernay <boblehamster@yahoo.fr>
2017-01-10 11:32:57 +0100

    correctif

--- uniqueNumber.js
+++ uniqueNumber.js
@@ -1,9 +1,11 @@
-var chaine = [ 0, 0, 0.55, 0, 0, 2 ];
+var chaine = [ 0, 0, 0.55, 0, 0, 2, 0.55 ];
 var found = false;
+var removedList = [];
 while (!found){
   var removed = chaine.splice(0,1);
-  if (chaine.indexOf(removed[0]) == -1){
-    found = true;
+  if (chaine.indexOf(removed[0]) == -1 && removedList.indexOf(removed[0]) == -1){
     console.log(removed[0]);
+    found = true;
   }
+  removedList.push(removed[0]);
 }
